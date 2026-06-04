0640955b737d7ac6a2de4df3774fd3f0024b56e4
Ari Victor <11803028+arivictor@users.noreply.github.com>
2026-06-04 18:09:21 +1000

    feat: update gomark dependency to v0.1.4, remove local settings, and enhance site runner configuration with bearer token

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/arivictor/intentional-code
 
 go 1.24.4
 
-require github.com/arivictor/gomark v0.1.1
+require github.com/arivictor/gomark v0.1.4
--- main.go
+++ main.go
@@ -13,13 +13,15 @@ func main() {
 		runnerURL = "http://localhost:8081"
 	}
 
+	secret := os.Getenv("RUNNER_AUTH_TOKEN")
+
 	s := gm.NewSite(
 		gm.WithSiteLogo("favicon-32x32.png"),
 		gm.WithSiteTitle("Intentional Code"),
 		gm.WithSiteURL("https://intentionalcode.com"),
 		gm.WithSiteContentDir("content"),
 		gm.WithSiteMode(gm.PreRender),
-		gm.WithSiteRunner(runnerURL, gm.AuthNone, ""),
+		gm.WithSiteRunner(runnerURL, gm.AuthBearerStatic, secret),
 		gm.WithSitePublicDir("public"),
 		gm.WithSiteOGImage("og-image.png"),
     	gm.WithSiteTwitterImage("twitter-image.png"),
